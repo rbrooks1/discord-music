6935fa92fb49a7c77385ccf9d640e9a2165961ae
Ryan Brooks <rbrook1598@gmail.com>
2021-06-21 13:35:32 -0600

    error handling and checks

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ function play(conn, msg) {
 	});
 };
 
+function validYtLink(url) {
+	var regex = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+	var match = url.match(regex);
+	return match[1];
+}
+
 client.on('message', async message => {
 	// Join the same voice channel of the author of the message
 
@@ -36,52 +42,91 @@ client.on('message', async message => {
 	const arg = body.split(' ');
 	const msg = arg.shift().toLowerCase();
 	if (message.member.voice.channel && msg === "play" && playing === 0) {
-		const connection = await message.member.voice.channel.join();
-		if (ytpl.validateID(String(arg))) {
-			const music = await ytpl(String(arg));
-			var items = music.items;
-			for (var i = 0; i < items.length; i++) {
-				var URL = items[i].shortUrl;
-				queue.push(URL);
+		if (arg.length === 1 && validYtLink(String(arg))) {
+			const connection = await message.member.voice.channel.join();
+			if (ytpl.validateID(String(arg))) {
+				const music = await ytpl(String(arg));
+				if (music === null || music === undefined) {
+					message.channel.send("Could not find playlist");
+				} else {
+					var items = music.items;
+					for (var i = 0; i < items.length; i++) {
+						var URL = items[i].shortUrl;
+						queue.push(URL);
+					}
+				}
+			} else if (String(arg).includes("start_radio")) {
+				const id = getVideoId(String(arg));
+				var mix = await ytmpl(id.id);
+				if (mix === null || mix === undefined) {
+					message.channel.send("Could not find playlist");
+				} else {
+					var items = mix.items;
+					for (var i = 0; i < items.length; i++) {
+						var URL = items[i].shortUrl;
+						queue.push(URL);
+					}
+				}
+			} else {
+				queue.push(arg);
 			}
-		} else if (String(arg).includes("start_radio")) {
-			const id = getVideoId(String(arg));
-			var mix = await ytmpl(id.id);
-			var items = mix.items;
-			for (var i = 0; i < items.length; i++) {
-				var URL = items[i].url;
-				queue.push(URL);
+			if (queue.length > 0) {
+				await play(connection, message);
 			}
 		} else {
-			queue.push(arg);
+			message.channel.send("Not a valid command");
 		}
-		await play(connection, message);
 	} else if (message.member.voice.channel && msg === "play" && playing === 1) {
-		message.channel.send("Queued " + arg);
-		if (ytpl.validateID(String(arg))) {
-			const music = await ytpl(String(arg));
-			var items = music.items;
-			for (var i = 0; i < items.length; i++) {
-				var URL = items[i].shortUrl;
-				queue.push(URL);
-			}
-		} else if (String(arg).includes("start_radio")) {
-			const id = getVideoId(String(arg));
-			var mix = await ytmpl(id.id);
-			var items = mix.items;
-			for (var i = 0; i < items.length; i++) {
-				var URL = items[i].url;
-				queue.push(URL);
+		if (arg.length === 1 && validYtLink(String(arg))) {
+			message.channel.send("Queued " + arg);
+			const connection = await message.member.voice.channel.join();
+			if (ytpl.validateID(String(arg))) {
+				const music = await ytpl(String(arg));
+				if (music === null || music === undefined) {
+					message.channel.send("Could not find playlist");
+				} else {
+					var items = music.items;
+					for (var i = 0; i < items.length; i++) {
+						var URL = items[i].shortUrl;
+						queue.push(URL);
+					}
+				}
+			} else if (String(arg).includes("start_radio")) {
+				const id = getVideoId(String(arg));
+				var mix = await ytmpl(id.id);
+				if (mix === null || mix === undefined) {
+					message.channel.send("Could not find playlist");
+				} else {
+					var items = mix.items;
+					for (var i = 0; i < items.length; i++) {
+						var URL = items[i].shortUrl;
+						queue.push(URL);
+					}
+				}
+			} else {
+				queue.push(arg);
 			}
 		} else {
-			queue.push(arg);
+			message.channel.send("Not a valid command");
 		}
 	} else if (message.member.voice.channel && msg === "disconnect") {
-		queue = [];
-		playing = 0;
-		message.member.voice.channel.leave();
+		if (arg === null || arg === undefined || arg.length === 0) {
+			queue = [];
+			playing = 0;
+			message.member.voice.channel.leave();
+		} else {
+			message.channel.send("Not a valid command");
+		}
 	} else if (message.member.voice.channel && msg === "queue") {
-		message.channel.send(queue);
+		if (arg === null || arg === undefined || arg.length === 0) {
+			if (queue.length > 0) {
+				message.channel.send(queue);
+			} else {
+				message.channel.send("No songs in queue");
+			}
+		} else {
+			message.channel.send("Not a valid command");
+		}
 	} else if (message.member.voice.channel && msg === "skip") {
 		if (queue.length > 1) {
 			const connection = await message.member.voice.channel.join();
@@ -90,6 +135,14 @@ client.on('message', async message => {
 		} else {
 			message.channel.send("On final song of queue");
 		}
+	} else if (message.member.voice.channel && msg === "help") {
+		if (arg === null || arg === undefined || arg.length === 0) {
+			message.channel.send("Here is a list of commands:\nplay\ndisconnect\nskip\nqueue");
+		} else {
+			message.channel.send("Not a valid command");
+		}
+	} else {
+		message.channel.send("This is not a valid command.\nType " + prefix + "help for a list of commands");
 	}
 });
 
